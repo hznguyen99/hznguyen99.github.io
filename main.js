66d00df9754aac9fdfef2ee69e3f1e714f0521b0
hznguyen99 <57025748+hznguyen99@users.noreply.github.com>
2019-10-26 16:30:58 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function playStream(idVideoTag, stream) {
 }
 
 //openStream().then(stream => playStream('localStream', stream));
-const peer = new Peer({key: 'peerjs', host: 'videocallvn.herokuapp.com', secure: true, port: 443});
+const peer = new Peer({host: 'videocallvn.herokuapp.com', secure: true, port: 443});
 peer.on('open',id => {
     $('#my-peer').append(id);
     $('#btnSignUp').click(() => {
